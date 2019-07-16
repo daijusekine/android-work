ee4390c239d9282c58fdf426f2ebfcc866671a4a
関根大樹 <mukku@sekinedaiki-no-iMac.local>
2019-07-17 00:12:20 +0900

    Add calculator

---
+++ Calculator/app/src/main/java/com/example/calculator/ResultActivity.kt
@@ -0,0 +1,25 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val price = intent.getIntExtra("price", 0)
+        val discount = intent.getIntExtra("discount", 0)
+
+        val expression = findViewById<TextView>(R.id.expression_label)
+        expression.text = getString(R.string.expression, price, discount)
+
+        val discountedPrice = price * (100 - discount) / 100
+
+        val result = findViewById<TextView>(R.id.result_label)
+        result.text = getString(R.string.result, discountedPrice)
+    }
+}
